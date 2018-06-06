08184560b39f3cab0711c63422fdc425b912db10
saisf <sleung82@mail.ccsf.edu>
2018-06-06 15:34:09 -0700

    Remove unnecessary Student constant class

--- On the map/Model/APIClient.swift
+++ On the map/Model/APIClient.swift
@@ -68,7 +68,6 @@ class APIClient: NSObject{
     
     // MARK: Verify if student has previously posted location
     func verifyUserLocationAlreadyExist(completion: @escaping (_ results: [[String:AnyObject]]?, _ error: Error?) -> Void) {
-        Student.Constant.activityIndicator(loading: true)
         var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=500&&order=-updatedAt")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
